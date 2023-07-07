640cdf5fed708ba667cf398262cb478a911e2c09
Anas-Iz <137615164+Anas-Iz@users.noreply.github.com>
2023-07-08 01:12:27 +0200

    Apperture Diameter

---
+++ CalcAppertureDiameter.m
@@ -0,0 +1,3 @@
+function D = CalcAppertureDiameter (h, lambda, GSD)
+D = (2.44*h*lambda)/GSD;
+end
---
+++ satellite.m
@@ -0,0 +1,7 @@
+1;
+
+% Apperture diameter
+lambda = 0.5*10^6; % in meter
+GSD = 50; % in m
+h = 750*10^3; % in m
+D = CalcAppertureDiameter(h,lambda,GSD);
